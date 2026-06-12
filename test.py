af4a4338fc333322a1ff8256ef0a2d9e43d7dcc0
Anubhab Sen <anubhabsen2007@gmail.com>
2026-06-12 22:03:54 +0530

    Dispersion Curve Picking
    Signed-off-by: Anubhab Sen <anubhabsen2007@gmail.com>

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from .Ridges import Map
+from Ridges import Map
 
 
 def test():
-    amp = np.loadtxt('write_amp.txt')
-    P = np.loadtxt('write_FP.txt')
-    V = np.loadtxt('write_TV.txt')
+    amp = np.loadtxt('DCPdata/write_amp.txt')
+    P = np.loadtxt('DCPdata/write_FP.txt')
+    V = np.loadtxt('DCPdata/write_TV.txt')
     M = Map(amp, P, V)
     M.setTol(0.2, 1)
     M.scan()
